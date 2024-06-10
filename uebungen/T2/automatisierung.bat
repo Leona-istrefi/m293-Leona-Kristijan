353d246f55ee4351e51283d4898dc84c04bfc901
Leona-istrefi <142780434+Leona-istrefi@users.noreply.github.com>
2024-06-10 09:43:48 +0200

    Update automatisierung.bat

--- uebungen/T2/automatisierung.bat
+++ uebungen/T2/automatisierung.bat
@@ -1,25 +1,6 @@
-@echo off
-setlocal
- 
-:: Konfigurationsabschnitt
-set SERVER=http://leonaistrefi.bplaced.net/
-set PORT=21
-set USERNAME=leona.istrefi@edu.tbz.ch
-set PASSWORD=Leeoona.08
-set LOCAL_PATH="C:\Users\TBZ\Documents\GitHub\m293-Leona-Kristijan\uebungen\H2\Uebung3"
-set REMOTE_PATH=/htdocs
- 
-:: Skriptabschnitt
-echo open %SERVER% %PORT% > ftpcmd.dat
-echo user %USERNAME% %PASSWORD% >> ftpcmd.dat
-echo binary >> ftpcmd.dat
-echo lcd %LOCAL_PATH% >> ftpcmd.dat
-echo cd %REMOTE_PATH% >> ftpcmd.dat
-echo mput * >> ftpcmd.dat
-echo bye >> ftpcmd.dat
- 
-ftp -s:ftpcmd.dat
- 
-del ftpcmd.dat
- 
-endlocal
+open http://leonaistrefi.bplaced.net/
+<leonaistrefi>
+<Leeoona.08>
+binary
+put ftp.txt
+bye
