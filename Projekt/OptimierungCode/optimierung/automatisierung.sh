7656007020234f70bde1f7164e8686d56a1f6f75
05kl <kristijan.lisica@edu.tbz.ch>
2024-07-09 08:33:58 +0200

    optimierung aufgabe b

--- Projekt/OptimierungCode/optimierung/automatisierung.sh
+++ Projekt/OptimierungCode/optimierung/automatisierung.sh
@@ -1,12 +1,41 @@
 #!/bin/bash
 
 # bestehenden Ziel-Dateien löschen
-rm ./dist/*.html
-rm ./dist/*.css
+rm ./dist/*
+rm -r ./dist/assets
+rm -r ./dist/fotos
 
+cp -r ./src/assets ./dist/assets
+cp -r ./src/fotos ./dist/fotos
 # minify
 npx html-minifier --input-dir ./src --output-dir ./dist --collapse-whitespace --file-ext html
 npx css-minify -d ./src -o ./dist
 # entferne .min.css
 for i in ./dist/*.min.css ; do mv "$i" "${i/.min.css/.css}" ; done
+
+#ftp
+HOST='kristijanlisica.bplaced.net'
+USER='kristijanlisica'
+PASSWD='>5W}N.qKE7&#j&F'
+FILE='test.txt'
+
+ftp -n $HOST <<END_SCRIPT
+quote USER $USER
+quote PASS $PASSWD
+binary
+cd www/carspage
+lcd dist
+prompt
+mput *
+cd fotos
+lcd fotos
+mput *
+cd ..
+lcd ..
+cd assets
+lcd assets
+mput *
+quit
+bye
+END_SCRIPT
 exit 0
